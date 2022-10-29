6b2beee70fa38ccfa92376f624d858ea62771b20
Ivan <ipkozyrin@gmail.com>
2022-10-29 08:06:45 +0300

    refactor

--- src/MainMenuHandler.ts
+++ src/MainMenuHandler.ts
@@ -1,9 +1,5 @@
 import TgChat from './tgApi/TgChat';
 import {askMainMenu} from './askUser/askMainMenu';
-// import {askPublishType} from './askUser/askPublishType';
-// import {askSNs} from './askUser/askSNs';
-import {PublicationTypes} from './types/types';
-import {PUBLICATION_TYPES} from './types/consts';
 import {askChannelMenu, MENU_MAKE_STORY, MENU_PUBLISH} from './askUser/askChannelMenu';
 import PublishMaterial from './publish/PublishMaterial';
 
@@ -36,47 +32,7 @@ export default class MainMenuHandler {
         }
       })
         .catch((e) => {throw e});
-
-      // askPublishType(channelId, this.tgChat, (pubType: PublicationTypes) => {
-      //   askSNs(channelId, pubType, this.tgChat, (sns: string[]) => {
-      //     this.startMakingRecord(channelId, pubType, sns)
-      //       .catch((e) => {throw e});
-      //   })
-      //     .catch((e) => {throw e});
-      // })
-      //   .catch((e) => {throw e});
     });
-  }
-
-
-  private async startMakingRecord(channelId: number, pubType: PublicationTypes, sns: string[]) {
-    // switch (pubType) {
-    //   case PUBLICATION_TYPES.article:
-    //     const article = new PublishArticle(channelId, sns, this.tgChat);
-    //
-    //     await article.start(channelId);
-    //     break;
-    //
-    //   case PUBLICATION_TYPES.post1000:
-    //     const post1000 = new PublishPost1000(channelId, sns, this.tgChat);
-    //
-    //     await post1000.start(channelId);
-    //     break;
-    //
-    //   // TODO: add post 2000
-    //
-    //   case PUBLICATION_TYPES.story:
-    //     const story = new PublishStory(channelId, sns, this.tgChat);
-    //
-    //     await story.start(channelId);
-    //     break;
-    //
-    //   default:
-    //     throw new Error(`Incorrect publish type`);
-    //
-    //     break;
-    // }
-
   }
 
 }
