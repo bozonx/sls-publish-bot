f9f40b493889227b5d894ca2075450ff71ebba97
Ivan <ipkozyrin@gmail.com>
2022-11-08 22:02:22 +0300

    change

--- src/publish/startPublishFromContentPlan.ts
+++ src/publish/startPublishFromContentPlan.ts
@@ -12,6 +12,8 @@ import {loadPageProps} from '../notionRequests/pageProps';
 import RawPageContent from '../types/PageContent';
 import {prepareFooterPost} from '../helpers/helpers';
 import {getFirstImageFromNotionBlocks, makeContentLengthString} from './publishHelpers';
+import {askPostConfirm} from '../askUser/askPostConfirm';
+import {NOTION_BLOCKS} from '../types/types';
 
 
 export async function startPublishFromContentPlan(blogName: string, tgChat: TgChat) {
@@ -22,9 +24,16 @@ export async function startPublishFromContentPlan(blogName: string, tgChat: TgCh
     try {
       const parsedContentItem = await prepareContentItem(item, blogName, tgChat);
       const parsedPage = await preparePage(parsedContentItem, blogName, tgChat);
+
+      if (!parsedPage && parsedContentItem.type !== PUBLICATION_TYPES.announcement) {
+        // if not nested page and it isn't announcement
+        await tgChat.reply(tgChat.app.i18n.errors.noNestedPage);
+        await tgChat.steps.back();
+      }
+
       const mainImgUrl = getFirstImageFromNotionBlocks(parsedPage?.textBlocks);
 
-      await printAllDetails(blogName, tgChat, parsedContentItem, parsedPage, mainImgUrl);
+      await printItemDetails(blogName, tgChat, parsedContentItem, parsedPage, mainImgUrl);
       await askMenu(blogName, tgChat, parsedContentItem, parsedPage, mainImgUrl);
     }
     catch (e) {
@@ -68,26 +77,21 @@ async function preparePage(
   const pageContent = await loadPageBlocks(parsedContentItem.relativePageId, tgChat);
   const parsedPage = parsePageContent(pageProperties, pageContent);
 
-  validatePageItem(parsedPage)
+  validatePageItem(parsedPage);
 
   return parsedPage;
 }
 
-async function printAllDetails(
+async function printItemDetails(
   blogName: string,
   tgChat: TgChat,
   parsedContentItem: ContentItem,
   parsedPage?: RawPageContent,
   mainImgUrl?: string
 ) {
-  if (mainImgUrl) {
-    try {
-      await tgChat.app.tg.bot.telegram.sendPhoto(tgChat.botChatId, mainImgUrl)
-    }
-    catch (e) {
-      await tgChat.reply(tgChat.app.i18n.errors.cantSendImage)
-    }
-  }
+  await printImage(blogName, tgChat, mainImgUrl);
+  const footerStr = await printFooter(blogName, tgChat, true, parsedPage?.tgTags);
+
   // make content plan info details message
   const contentInfoMsg = makeContentPlanItemDetails(parsedContentItem, tgChat.app.i18n);
   // send record's info from content plan
@@ -102,14 +106,53 @@ async function printAllDetails(
     await tgChat.reply(
       tgChat.app.i18n.menu.pageContent + '\n\n' + pageDetailsMsg
     );
-
-    const tgFooter = prepareFooterPost(
-      tgChat.app.config.blogs[blogName].sn.telegram?.postFooter,
-      parsedPage?.tgTags
+  }
+  // TODO: наверное перенести в printPublicationContent
+  else if (parsedContentItem.type === PUBLICATION_TYPES.announcement) {
+    // don't have the nested page and it is annoucement
+    // TODO: проверить что для обьявления выбран telegram
+    await tgChat.reply(
+      tgChat.app.i18n.menu.announcementGist + '\n' + parsedContentItem.gist
     );
+    // TODO: может тоже подсчитать длину???
+  }
+
+  await printContent(
+    blogName,
+    tgChat,
+    parsedPage?.textBlocks,
+    parsedContentItem.gist,
+    parsedPage?.tgTags,
+    parsedPage?.instaTags,
+    footerStr,
+  );
+}
+
+async function printImage(blogName: string, tgChat: TgChat, mainImgUrl?: string) {
+  if (mainImgUrl) {
+    try {
+      await tgChat.app.tg.bot.telegram.sendPhoto(tgChat.botChatId, mainImgUrl)
+    }
+    catch (e) {
+      await tgChat.reply(tgChat.app.i18n.errors.cantSendImage)
+    }
+  }
+}
+
+async function printFooter(
+  blogName: string,
+  tgChat: TgChat,
+  useFooter: boolean,
+  tgTags?: string[]
+): Promise<string | undefined> {
+  let tgFooter: string | undefined;
+  const postFooter = tgChat.app.config.blogs[blogName].sn.telegram?.postFooter;
+
+  if (useFooter) {
+    tgFooter = prepareFooterPost(postFooter, tgTags);
 
     // print footer
-    if (tgChat.app.config.blogs[blogName].sn.telegram?.postFooter) {
+    if (postFooter) {
       await tgChat.reply(
         tgChat.app.i18n.menu.postFooter + tgFooter,
         undefined,
@@ -117,25 +160,33 @@ async function printAllDetails(
         true
       );
     }
+  }
 
-    // tgChat.app.i18n.commonPhrases.selectedNoPreview + tgChat.app.i18n.onOff[1] + '\n'
+  return tgFooter;
+}
 
-    if (parsedPage) {
-      await tgChat.reply(makeContentLengthString(tgChat.app.i18n, parsedPage, tgFooter));
-    }
+async function printContent(
+  blogName: string,
+  tgChat: TgChat,
+  textBlocks?: NOTION_BLOCKS,
+  postText?: string,
+  tgTags?: string[],
+  instaTags?: string[],
+  footerStr?: string,
+) {
+  if (textBlocks) {
+    await tgChat.reply(makeContentLengthString(
+      tgChat.app.i18n,
+      textBlocks,
+      tgTags,
+      instaTags,
+      footerStr
+    ));
   }
   else {
-    if (parsedContentItem.type === PUBLICATION_TYPES.announcement) {
-      // TODO: проверить что для обьявления выбран telegram
-      await tgChat.reply(
-        tgChat.app.i18n.menu.announcementGist + '\n' + parsedContentItem.gist
-      );
-    }
-    else {
-      await tgChat.reply(tgChat.app.i18n.errors.noNestedPage);
-
-      await tgChat.steps.back();
-    }
+    await tgChat.reply(
+      tgChat.app.i18n.menu.announcementGist + '\n' + postText
+    );
   }
 }
 
@@ -159,23 +210,40 @@ async function askMenu(
   };
 
   await askPublishMenu(blogName, tgChat, state, tgChat.asyncCb(async () => {
+    // TODO: sns
+    // TODO: date and time
+    // TODO: use preview
+    // tgChat.app.i18n.commonPhrases.selectedNoPreview + tgChat.app.i18n.onOff[1] + '\n'
 
-    // TODO: может на всё обрабатывать ошибку, написать пользвателю и сделать back()
-    // TODO: может сделать доп подтверждение с предпросмотром???
-    // TODO: нужно обработать ошибку и написать пользователю
+    await printImage(blogName, tgChat, mainImgUrl);
+    const footerStr = await printFooter(blogName, tgChat, state.useFooter, parsedPage?.tgTags);
 
-    // Do publish
-    // await publishFork(
-    //   blogName,
-    //   tgChat,
-    //   parsedContentItem,
-    //   state.usePreview,
-    //   state.useFooter,
-    //   state.selectedTime,
-    //   parsedPage,
-    // );
+    await printContent(
+      blogName,
+      tgChat,
+      parsedPage?.textBlocks,
+      state.postText,
+      parsedPage?.tgTags,
+      parsedPage?.instaTags,
+      footerStr,
+    );
 
-    await tgChat.reply(tgChat.app.i18n.message.taskRegistered)
-    await tgChat.steps.cancel();
+    await askPostConfirm(blogName, tgChat, tgChat.asyncCb(async () => {
+      // TODO: может на всё обрабатывать ошибку, написать пользвателю и сделать back()
+      // TODO: нужно обработать ошибку и написать пользователю
+      // Do publish
+      await publishFork(
+        blogName,
+        tgChat,
+        parsedContentItem,
+        state.usePreview,
+        state.useFooter,
+        state.selectedTime,
+        parsedPage,
+      );
+
+      await tgChat.reply(tgChat.app.i18n.message.taskRegistered)
+      await tgChat.steps.cancel();
+    }));
   }));
 }
