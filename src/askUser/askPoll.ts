e120cce3e7ee09757db87f785bb0085777055531
Ivan <ipkozyrin@gmail.com>
2022-11-12 21:42:27 +0300

    add

--- src/askUser/askPoll.ts
+++ src/askUser/askPoll.ts
@@ -1,11 +1,13 @@
 import TgChat from '../apiTg/TgChat';
 import {
+  AppEvents,
   BACK_BTN,
   BACK_BTN_CALLBACK,
   CANCEL_BTN,
   CANCEL_BTN_CALLBACK,
 } from '../types/constants';
-import {addSimpleStep} from '../helpers/helpers';
+import BaseState from '../types/BaseState';
+import {PollMessageEvent} from '../types/MessageEvent';
 
 
 export async function askPoll(tgChat: TgChat, onDone: () => void) {
@@ -17,14 +19,36 @@ export async function askPoll(tgChat: TgChat, onDone: () => void) {
     ],
   ];
 
-  await addSimpleStep(tgChat, msg, buttons,async (queryData: string) => {
-    if (queryData === BACK_BTN_CALLBACK) {
-      return tgChat.steps.back();
-    }
-    else if (queryData === CANCEL_BTN_CALLBACK) {
-      return tgChat.steps.cancel();
-    }
 
+  await tgChat.addOrdinaryStep(async (state: BaseState) => {
+    // print main menu message
+    state.messageIds.push(await tgChat.reply(msg, buttons));
+    // listen to result
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        AppEvents.CALLBACK_QUERY,
+        tgChat.asyncCb(async (queryData: string) => {
+          if (queryData === BACK_BTN_CALLBACK) {
+            return tgChat.steps.back();
+          }
+          else if (queryData === CANCEL_BTN_CALLBACK) {
+            return tgChat.steps.cancel();
+          }
+          // TODO: !!!!
+
+        })
+      ),
+      AppEvents.CALLBACK_QUERY
+    ]);
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        AppEvents.POLL,
+        tgChat.asyncCb(async (message: PollMessageEvent) => {
+
+        })
+      ),
+      AppEvents.TEXT
+    ]);
   });
 
 }
