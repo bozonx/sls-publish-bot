677dbed7221c514f6541bed53fe51acce28afb87
Ivan <ipkozyrin@gmail.com>
2022-11-15 20:28:13 +0300

    refactor

--- src/askUser/askMainMenu.ts
+++ src/askUser/askMainMenu.ts
@@ -1,6 +1,5 @@
-import BaseState from '../types/BaseState';
-import {ChatEvents} from '../types/constants';
 import TgChat from '../apiTg/TgChat';
+import {addSimpleStep} from '../helpers/helpers';
 
 
 export const MAIN_MENU_ACTION = {
@@ -13,46 +12,8 @@ const BLOG_MARKER = 'blog:';
 
 
 export async function askMainMenu(tgChat: TgChat, onDone: (blogNameOrAction: string) => void) {
-  await tgChat.addOrdinaryStep(async (state: BaseState) => {
-    // print main menu message
-    state.messageIds.push(await printInitialMessage(tgChat));
-    // listen to result
-    state.handlerIndexes.push([
-      tgChat.events.addListener(
-        ChatEvents.CALLBACK_QUERY,
-        tgChat.asyncCb(async (queryData: string) => {
-          if (queryData.indexOf(BLOG_MARKER) === 0) {
-            await blogSelected(queryData, tgChat, onDone);
-          }
-          else {
-            onDone(queryData);
-          }
-        }
-      )),
-      ChatEvents.CALLBACK_QUERY
-    ]);
-  });
-}
-
-async function blogSelected(
-  queryData: string,
-  tgChat: TgChat,
-  onDone: (blogNameOrAction: string) => void
-) {
-  const splat: string[] = queryData.split(':');
-  const blogName: string = splat[1];
-
-  // print result
-  await tgChat.reply(
-    tgChat.app.i18n.menu.selectedBlog
-    + tgChat.app.config.blogs[blogName].dispname
-  );
-
-  onDone(blogName);
-}
-
-async function printInitialMessage(tgChat: TgChat): Promise<number> {
-  return tgChat.reply(tgChat.app.i18n.menu.mainMenu, [
+  const msg = tgChat.app.i18n.menu.mainMenu;
+  const buttons = [
     Object.keys(tgChat.app.config.blogs).map((blogName): any => {
       return {
         text: tgChat.app.config.blogs[blogName].dispname,
@@ -75,5 +36,23 @@ async function printInitialMessage(tgChat: TgChat): Promise<number> {
         callback_data: MAIN_MENU_ACTION.TASKS,
       }
     ]
-  ]);
+  ];
+
+  await addSimpleStep(tgChat, msg, buttons, tgChat.asyncCb(async (queryData: string) => {
+    if (queryData.indexOf(BLOG_MARKER) === 0) {
+      const splat: string[] = queryData.split(':');
+      const blogName: string = splat[1];
+
+      // print result
+      await tgChat.reply(
+        tgChat.app.i18n.menu.selectedBlog
+        + tgChat.app.config.blogs[blogName].dispname
+      );
+
+      onDone(blogName);
+    }
+    else {
+      onDone(queryData);
+    }
+  }));
 }
