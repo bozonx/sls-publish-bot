c874fa54021f23c957fa3f1376083295ed2b5468
Ivan <ipkozyrin@gmail.com>
2022-11-18 21:51:23 +0300

    change

--- src/askUser/askSiteMenu.ts
+++ src/askUser/askSiteMenu.ts
@@ -1,32 +1,20 @@
 import TgChat from '../apiTg/TgChat';
-import BaseState from '../types/BaseState';
-import {ChatEvents, CANCEL_BTN, CANCEL_BTN_CALLBACK} from '../types/constants';
+import {CANCEL_BTN, CANCEL_BTN_CALLBACK} from '../types/constants';
+import {addSimpleStep} from '../helpers/helpers';
 
 
 export async function askSiteMenu(tgChat: TgChat, onDone: () => void) {
-  await tgChat.addOrdinaryStep(async (state: BaseState) => {
-    // print main menu message
-    state.messageIds.push(await printInitialMessage(tgChat));
-    // listen to result
-    state.handlerIndexes.push([
-      tgChat.events.addListener(
-        ChatEvents.CALLBACK_QUERY,
-        tgChat.asyncCb(async (queryData: string) => {
-            if (queryData === CANCEL_BTN_CALLBACK) {
-              return tgChat.steps.cancel();
-            }
-            // else do nothing
-          }
-        )),
-      ChatEvents.CALLBACK_QUERY
-    ]);
-  });
-}
-
-async function printInitialMessage(tgChat: TgChat): Promise<number> {
-  return tgChat.reply(tgChat.app.i18n.menu.siteMenu, [
+  const msg = tgChat.app.i18n.menu.siteMenu;
+  const buttons = [
     [
       CANCEL_BTN,
     ]
-  ]);
+  ];
+
+  await addSimpleStep(tgChat, msg, buttons,async (queryData: string) => {
+    if (queryData === CANCEL_BTN_CALLBACK) {
+      return tgChat.steps.cancel();
+    }
+    // else do nothing
+  });
 }
