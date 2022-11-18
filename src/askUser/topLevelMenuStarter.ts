c3c3c408300e95eac5baca11040825c5076da2e7
Ivan <ipkozyrin@gmail.com>
2022-11-18 19:27:33 +0300

    refactor

--- src/askUser/topLevelMenuStarter.ts
+++ src/askUser/topLevelMenuStarter.ts
@@ -1,9 +1,5 @@
 import TgChat from '../apiTg/TgChat';
 import {askMainMenu, MAIN_MENU_ACTION} from './askMainMenu';
-import {askSiteMenu} from './askSiteMenu';
-import {askTasksListMenu} from './askTasksListMenu';
-import {askTaskMenu} from './askTaskMenu';
-import {askTelegraphMenu, TELEGRAPH_MENU, TelegraphMenu} from './askTelegraphMenu';
 import {startBlogMenu} from './startBlogMenu';
 import {startTaskMenu} from './startTaskMenu';
 import {startSiteMenu} from './startSiteMenu';
@@ -12,17 +8,15 @@ import {startTelegraphMenu} from './startTelegraphMenu';
 
 export async function topLevelMenuStarter(tgChat: TgChat) {
   return askMainMenu(tgChat, tgChat.asyncCb(async (blogNameOrAction: string) => {
-    if (blogNameOrAction === MAIN_MENU_ACTION.SITE) {
-      await startSiteMenu(tgChat);
-    }
-    else if (blogNameOrAction === MAIN_MENU_ACTION.TASKS) {
-      await startTaskMenu(tgChat);
-    }
-    else if (blogNameOrAction === MAIN_MENU_ACTION.TELEGRAPH) {
-      await startTelegraphMenu(tgChat);
-    }
-    else {
-      return startBlogMenu(blogNameOrAction, tgChat);
+    switch (blogNameOrAction) {
+      case MAIN_MENU_ACTION.SITE:
+        return startSiteMenu(tgChat);
+      case MAIN_MENU_ACTION.TASKS:
+        return startTaskMenu(tgChat);
+      case MAIN_MENU_ACTION.TELEGRAPH:
+        return startTelegraphMenu(tgChat);
+      default:
+        return startBlogMenu(blogNameOrAction, tgChat);
     }
   }));
 }
