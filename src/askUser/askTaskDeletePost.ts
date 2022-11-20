2a69b9104523b754044f44a553756fc1225a1823
Ivan <ipkozyrin@gmail.com>
2022-11-20 19:07:48 +0300

    add

---
+++ src/askUser/askTaskDeletePost.ts
@@ -0,0 +1,87 @@
+import TgChat from '../apiTg/TgChat';
+import {BACK_BTN, BACK_BTN_CALLBACK, CANCEL_BTN, CANCEL_BTN_CALLBACK, ChatEvents, OK_BTN} from '../types/constants';
+import BaseState from '../types/BaseState';
+import {PhotoMessageEvent, PollMessageEvent, TextMessageEvent, VideoMessageEvent} from '../types/MessageEvent';
+import {askDateTime} from './askDateTime';
+import {makeUtcOffsetStr} from '../helpers/helpers';
+
+
+type OnDoneType = (messageId: number, chatId: number, startTime: string) => void;
+
+
+export async function askTaskDeletePost(tgChat: TgChat, onDone: OnDoneType) {
+  const msg = tgChat.app.i18n.menu.taskDeletePost;
+  const buttons = [
+    [
+      BACK_BTN,
+      CANCEL_BTN,
+    ]
+  ];
+
+  await tgChat.addOrdinaryStep(async (state: BaseState) => {
+    // print main menu message
+    state.messageIds.push(await tgChat.reply(msg, buttons));
+    // listen to text message
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        ChatEvents.TEXT,
+        tgChat.asyncCb(async (textMsg: TextMessageEvent) => {
+          await handleIncomeMessage(textMsg.messageId, textMsg.chatId, tgChat, onDone);
+        })
+      ),
+      ChatEvents.TEXT,
+    ]);
+    // listen to photo message
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        ChatEvents.PHOTO,
+        tgChat.asyncCb(async (photoMsg: PhotoMessageEvent) => {
+          await handleIncomeMessage(photoMsg.messageId, photoMsg.chatId, tgChat, onDone);
+        })
+      ),
+      ChatEvents.PHOTO,
+    ]);
+    // listen to video message
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        ChatEvents.VIDEO,
+        tgChat.asyncCb(async (videoMsg: VideoMessageEvent) => {
+          await handleIncomeMessage(videoMsg.messageId, videoMsg.chatId, tgChat, onDone);
+        })
+      ),
+      ChatEvents.VIDEO,
+    ]);
+    // listen to poll message
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        ChatEvents.POLL,
+        tgChat.asyncCb(async (pollMsg: PollMessageEvent) => {
+          await handleIncomeMessage(pollMsg.messageId, pollMsg.chatId, tgChat, onDone);
+        })
+      ),
+      ChatEvents.POLL,
+    ]);
+    // listen to buttons
+    state.handlerIndexes.push([
+      tgChat.events.addListener(
+        ChatEvents.CALLBACK_QUERY,
+        tgChat.asyncCb(async (cbData: string) => {
+          if (cbData === CANCEL_BTN_CALLBACK) {
+            await tgChat.steps.cancel();
+          }
+          else if (cbData === BACK_BTN_CALLBACK) {
+            await tgChat.steps.back();
+          }
+        })
+      ),
+      ChatEvents.CALLBACK_QUERY
+    ]);
+  });
+}
+
+
+async function handleIncomeMessage(messageId: number, chatId: number, tgChat: TgChat, onDone: OnDoneType) {
+  await askDateTime(tgChat, (isoDate: string, time: string) => {
+    onDone(messageId, chatId, `${isoDate}T${time}Z${makeUtcOffsetStr(tgChat.app.appConfig.utcOffset)}`)
+  });
+}
--- src/askUser/startTaskMenu.ts
+++ src/askUser/startTaskMenu.ts
@@ -1,14 +1,29 @@
 import {askTasksListMenu, TASK_LIST_ACTIONS} from './askTasksListMenu';
 import {askTaskMenu} from './askTaskMenu';
 import TgChat from '../apiTg/TgChat';
+import {askTaskDeletePost} from './askTaskDeletePost';
 
 
 export async function startTaskMenu(tgChat: TgChat) {
   await askTasksListMenu(
     tgChat,
-    (taskId?: string, action?: string) => {
+    tgChat.asyncCb(async (taskId?: string, action?: string) => {
       if (action === TASK_LIST_ACTIONS.DELETE_POST) {
+        return askTaskDeletePost(tgChat, tgChat.asyncCb(async (
+          messageId: number,
+          chatId: number,
+          startTime: string
+        ) => {
+          await tgChat.app.tasks.addTask({
+            messageId,
+            chatId,
+            sn: 'telegram',
+            startTime,
+            type: 'deletePost',
+          });
 
+          await tgChat.steps.cancel();
+        }));
       }
       else if (action === TASK_LIST_ACTIONS.PIN_POST) {
 
@@ -20,10 +35,10 @@ export async function startTaskMenu(tgChat: TgChat) {
 
       }
       else if (taskId) {
-        askTaskMenu(taskId, tgChat, () => {
-          tgChat.steps.cancel();
-        });
+        return askTaskMenu(taskId, tgChat, tgChat.asyncCb(async () => {
+          await tgChat.steps.cancel();
+        }));
       }
-    }
+    })
   );
 }
