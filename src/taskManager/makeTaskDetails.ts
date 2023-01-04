dc5a5a58a0068f4c6adfc75805f377c365c5e2e5
Ivan <ipkozyrin@gmail.com>
2023-01-04 13:02:06 +0300

    change

--- src/taskManager/makeTaskDetails.ts
+++ src/taskManager/makeTaskDetails.ts
@@ -7,32 +7,31 @@ import App from '../App.js';
 export async function makeTaskDetails(task: TaskItem, app: App): Promise<string> {
   let result = `${app.i18n.commonPhrases.type}: ${task.type}\n`
     + `${app.i18n.commonPhrases.date}: ${moment(task.startTime).format(PRINT_SHORT_DATE_TIME_FORMAT)}\n`;
-  let chatName: string | undefined;
-
-  if (task.chatId) {
-    // const res = await app.tg.bot.telegram.getChat(task.chatId)
-    //
-    // chatName = res.
-  }
+  let username: string | undefined;
 
   if (task.sn) result += `${app.i18n.commonPhrases.sn}: ${task.sn}`;
   if (task.chatId) {
+    const res = await app.tg.bot.telegram.getChat(task.chatId)
+
+    username = (res as any).username
+
     result += `chatId: ${task.chatId}`;
+    result += `username: ${username}`;
   }
 
   if ((task as any).messageId) {
     result += `messageId: ${(task as any).messageId}`;
 
-    if (chatName) {
-      result += `message url: https://t.me/${chatName}/${(task as any).messageId}`;
+    if (username) {
+      result += `message url: https://t.me/${username}/${(task as any).messageId}`;
     }
   }
 
   if ((task as any).forwardMessageId) {
     result += `forwardMessageId: ${(task as any).forwardMessageId}`;
 
-    if (chatName) {
-      result += `forwardMessage url: https://t.me/${chatName}/${(task as any).forwardMessageId}`;
+    if (username) {
+      result += `forwardMessage url: https://t.me/${username}/${(task as any).forwardMessageId}`;
     }
   }
 
