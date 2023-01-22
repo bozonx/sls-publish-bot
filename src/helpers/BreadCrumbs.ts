b0233c37c60f827692d7786b17a5c79e0143abc2
Ivan <ipkozyrin@gmail.com>
2023-01-22 09:56:08 +0300

    add

--- src/helpers/BreadCrumbs.ts
+++ src/helpers/BreadCrumbs.ts
@@ -1,22 +1,19 @@
-
-
-// TODO: review, refactor
-
-
 export interface BreadCrumbsStep {
   // It will be called when step is started
-  onStart(state: Record<string, any>): Promise<void>;
+  onStart(state: Record<string, any>): Promise<void>
   // It will be called when step is finished (the next is added)
-  onEnd(state: Record<string, any>): Promise<void>;
-  // It will be called on canceling step
-  onCancel(state: Record<string, any>): Promise<void>;
-  state: Record<string, any>;
+  onEnd(state: Record<string, any>): Promise<void>
+  // It will be called on canceling step or if is going to switch to previous one
+  onCancel(state: Record<string, any>): Promise<void>
+  state: Record<string, any>
+  // optional step name which is used to switch to certain step
+  name?: string
 }
 
 
 export default class BreadCrumbs {
-  private steps: BreadCrumbsStep[] = [];
-  private readonly initialStep: () => Promise<void>;
+  private readonly steps: BreadCrumbsStep[] = []
+  private readonly initialStep: () => Promise<void>
 
 
   constructor(initialStep: () => Promise<void>) {
@@ -25,7 +22,9 @@ export default class BreadCrumbs {
   }
 
   destroy() {
-    // TODO: what to do ????
+    // clear all the steps
+    this.deleteStepAndAfter(0)
+    //delete this.initialStep
   }
 
 
@@ -44,87 +43,100 @@ export default class BreadCrumbs {
       if (stepNum > 0) {
         await this.justEndStep(stepNum - 1);
       }
-
+      // run current step
       await this.justExecuteStep(stepNum);
     }
     catch (e) {
+      // TODO: review
       // TODO: или может запустить stop. Или разрешить ошибку
-      this.deleteStepAndAfter(stepNum);
+      this.deleteStepAndAfter(stepNum)
 
-      throw e;
+      throw e
     }
 
-    return stepNum;
+    return stepNum
   }
+
+  /**
+   * Switch to certain step by it's id.
+   * It removes all the other steps which are after it
+   */
+  async to(stepName: string) {
+    const stepIndex = this.steps.findIndex((e) => e.name === stepName);
+
+    if (stepIndex < 0) {
+      return this.cancel()
+    }
+
+    return this.runStepByIndex(stepIndex)
+  }
+
+  async back() {
+    // go to initial if no steps
+    if (this.steps.length < 1) return this.initialStep()
+
+    const currentStepIndex = this.steps.length - 1
+    // cancel current step
+    await this.justCancelStep(currentStepIndex)
+    // remove current step
+    this.deleteStepAndAfter(currentStepIndex)
+
+    const newCurrentStepIndex = this.steps.length - 1
+
+    if (this.steps.length) {
+      // if it has some other steps - run the last
+      return this.justExecuteStep(newCurrentStepIndex)
+    }
+    // if no steps - run initial step
+    return this.initialStep()
+  }
+
+  async cancel() {
+    if (this.steps.length) {
+      const currentStepIndex = this.steps.length - 1
+      // cancel current step
+      await this.justCancelStep(currentStepIndex)
+      // clear all the steps
+      this.deleteStepAndAfter(0)
+    }
+
+    await this.initialStep()
+  }
+
 
   /**
    * Run step which is in step stack.
    * Removes steps after specified and start it
    */
-  async runStep(stepNum: number) {
-    if (this.steps.length -1 < stepNum) {
-      // run one of previous steps
+  private async runStepByIndex(stepIndex: number) {
+    if (this.steps.length -1 < stepIndex) {
+      // run one of the previous steps
       // cancel currently running step
-      await this.justCancelStep(this.steps.length - 1);
+      await this.justCancelStep(this.steps.length - 1)
       // remove steps after specified
-      this.deleteStepAndAfter(stepNum + 1);
+      this.deleteStepAndAfter(stepIndex + 1)
       // run specified state
-      return this.justExecuteStep(stepNum);
+      return this.justExecuteStep(stepIndex)
     }
-    else if (this.steps.length -1 === stepNum) {
-      // run current step
-      // restart current step
+    else if (this.steps.length -1 === stepIndex) {
+      // restart current step which is the last step
       // just cancel current step
-      await this.justCancelStep(stepNum);
+      await this.justCancelStep(stepIndex)
       // just run current step
-      return this.justExecuteStep(stepNum);
+      return this.justExecuteStep(stepIndex)
     }
 
-    throw new Error(`Wrong stepNum ${stepNum}`);
+    throw new Error(`Wrong stepNum ${stepIndex}`)
   }
-
-  async back() {
-    // do nothing if no steps
-    if (this.steps.length < 1) return;
-
-    const currentStep = this.steps.length - 1;
-    // cancel current step
-    await this.justCancelStep(currentStep);
-    // remove current step
-    this.deleteStepAndAfter(currentStep);
-
-    const newCurrentStep = this.steps.length - 1;
-
-    if (this.steps.length) {
-      // if it has some other steps - run the last
-      await this.justExecuteStep(newCurrentStep);
-    }
-    else {
-      // if no steps - run initial step
-      await this.initialStep();
-    }
-  }
-
-  async cancel() {
-    if (this.steps.length) {
-      const currentStep = this.steps.length - 1;
-      // cancel current step
-      await this.justCancelStep(currentStep);
-      // clear all the steps
-      this.deleteStepAndAfter(0);
-    }
-
-    await this.initialStep();
-  }
-
 
   /**
    * Just delete step and those which after it
    */
-  private deleteStepAndAfter(stepNum: number) {
-    if (this.steps.length -1 < stepNum) return;
+  private deleteStepAndAfter(stepIndex: number) {
+    // if index less than specified
+    if (this.steps.length -1 < stepIndex) return
 
-    this.steps.splice(stepNum);
+    this.steps.splice(stepIndex)
   }
 
   private justExecuteStep(stepNum: number): Promise<void> {
