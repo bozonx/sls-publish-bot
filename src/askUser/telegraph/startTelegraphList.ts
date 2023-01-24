518fa16d43240028e073b4c2b84be98b86a39796
Ivan <ipkozyrin@gmail.com>
2023-01-24 19:41:16 +0300

    add

--- src/askUser/telegraph/startTelegraphList.ts
+++ src/askUser/telegraph/startTelegraphList.ts
@@ -1,12 +1,25 @@
 import {Page} from 'better-telegraph/src/types.js';
 import TgChat from '../../apiTg/TgChat.js';
-import {askTelegraphList, TelegraphListMenu} from './askTelegraphList.js';
+import {askTelegraphList} from './askTelegraphList.js';
+import {askConfirm} from '../common/askConfirm.js';
 
 
 export async function startTelegraphList(tgChat: TgChat) {
   await askTelegraphList(tgChat, tgChat.asyncCb(async (page: Page) => {
-    console.log(222222, page)
-    if ()
-    // TODO: what to do???
+
+    await tgChat.reply(makePageDetails(page), undefined, false, true)
+
+    await askConfirm(tgChat, () => {}, '🤔', true)
   }))
+}
+
+
+function makePageDetails(page: Page): string {
+  const result : string[] = []
+
+  result.push(`title<a href="${page.image_url}">:</a> <a href="${page.url}">${page.title}</a>`)
+  result.push(`description: ${page.description}`)
+  result.push(`views: ${page.views}`)
+
+  return result.join('\n')
 }
