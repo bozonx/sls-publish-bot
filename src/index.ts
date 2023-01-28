e0e4386837c9ab4801cf3d3c9f6b8edde98c490d
Ivan <ipkozyrin@gmail.com>
2023-01-28 15:42:03 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import * as fs from 'fs';
 import yaml from 'js-yaml'
 import dotenv from 'dotenv';
@@ -10,13 +11,22 @@ dotenv.config();
 if (!process.env.CONFIG_PATH) {
   console.error(`You need to set env var CONFIG_PATH`);
 
-  process.exit(1);
+  process.exit(2);
+}
+else if (!process.env.CHANNEL_IDS) {
+  console.error(`You need to set env var CHANNEL_IDS`);
+
+  process.exit(2);
 }
 
-const conf = yaml.load(
-  fs.readFileSync(process.env.CONFIG_PATH, 'utf8')
-) as BlogsConfig
-
+const confStr = fs.readFileSync(process.env.CONFIG_PATH, 'utf8')
+const finalCfgString = _.template(confStr)({
+  TAGS: '${ TAGS }',
+  TITLE: '${ TITLE }',
+  ARTICLE_URL: '${ ARTICLE_URL }',
+  ...JSON.parse(process.env.CHANNEL_IDS),
+})
+const conf = yaml.load(finalCfgString) as BlogsConfig
 const app = new App(conf)
 
 app.init()
