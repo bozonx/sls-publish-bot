bccc554313854880553e0f0a9c77ede9a1676da6
Ivan <ipkozyrin@gmail.com>
2023-02-09 11:33:31 +0300

    change

--- src/helpers/convertNotionToInstagramPost.ts
+++ src/helpers/convertNotionToInstagramPost.ts
@@ -7,89 +7,101 @@ import {TextRichTextItemResponse} from '@notionhq/client/build/src/api-endpoints
 
 export function convertNotionToInstagramPost(notionBlocks: NotionBlocks): string {
   let result = '';
-  let numberListCounter = 0;
-  let bulletedListCounter = 0;
+  let numberListCounter = 0
+  let bulletedListCounter = 0
 
   for (const block of notionBlocks) {
-    // skip images
-    if (block.type === NOTION_BLOCK_TYPES.image) continue;
+    let children: string = ''
 
-    if (block.has_children) {
-      // TODO: recurse
+    // skip images
+    if (block.type === NOTION_BLOCK_TYPES.image) continue
+
+    if ((block as any).children) {
+      children = convertNotionToInstagramPost((block as any).children)
     }
 
     if (block.type !== NOTION_BLOCK_TYPES.bulleted_list_item) {
       // if the end of ul block - put line break
-      if (bulletedListCounter > 0) result += '\n';
+      if (bulletedListCounter > 0) result += '\n'
 
-      bulletedListCounter = 0;
+      bulletedListCounter = 0
     }
 
     if (block.type !== NOTION_BLOCK_TYPES.numbered_list_item) {
       // if the end of ol block - put line break
-      if (numberListCounter > 0) result += '\n';
+      if (numberListCounter > 0) result += '\n'
 
-      numberListCounter = 0;
+      numberListCounter = 0
     }
 
     switch (block.type) {
-      // TODO: сделать жирным
       case NOTION_BLOCK_TYPES.heading_1:
-        result += richTextToSimpleTextList((block as any)?.heading_1?.rich_text) + '\n\n';
-        break;
+        result += richTextToSimpleTextList((block as any)?.heading_1?.rich_text) + '\n\n'
+
+        break
       case NOTION_BLOCK_TYPES.heading_2:
-        result += richTextToSimpleTextList((block as any)?.heading_2?.rich_text) + '\n\n';
-        break;
+        result += richTextToSimpleTextList((block as any)?.heading_2?.rich_text) + '\n\n'
+
+        break
       case NOTION_BLOCK_TYPES.heading_3:
-        result += richTextToSimpleTextList((block as any)?.heading_3?.rich_text) + '\n\n';
-        break;
+        result += richTextToSimpleTextList((block as any)?.heading_3?.rich_text) + '\n\n'
+
+        break
       case NOTION_BLOCK_TYPES.paragraph:
         if ((block as any)?.paragraph?.rich_text.length) {
-          result += richTextToInstaTextList((block as any)?.paragraph?.rich_text) + '\n\n';
+          result += richTextToInstaTextList((block as any)?.paragraph?.rich_text)
+            + children
+            + '\n\n'
         }
         else {
           // empty row
-          result += '\n';
+          result += '\n'
         }
 
         break;
       case NOTION_BLOCK_TYPES.bulleted_list_item:
-        bulletedListCounter++;
+        bulletedListCounter++
         result += `* `
           + richTextToInstaTextList((block as any)?.bulleted_list_item?.rich_text)
-          + '\n';
+          + ((children)
+            ? '\n  ' + children.replace(/\n/g, '\n  ')
+            : '')
+          + '\n'
 
-        break;
+        break
       case NOTION_BLOCK_TYPES.numbered_list_item:
         numberListCounter++;
         result += `${numberListCounter}. `
           + richTextToInstaTextList((block as any)?.numbered_list_item?.rich_text)
-          + '\n';
+          + ((children)
+            ? '\n  ' + children.replace(/\n/g, '\n  ')
+            : '')
+          + '\n'
 
-        break;
+        break
       case NOTION_BLOCK_TYPES.quote:
         result += `| `
           + richTextToInstaTextList((block as any)?.quote?.rich_text)
             .replace(/\n/g, '\n| ')
-          + '\n\n';
+          + '\n\n'
 
-        break;
+        break
       case NOTION_BLOCK_TYPES.code:
         result += richTextToInstaTextList((block as any)?.code?.rich_text)
-          + '\n\n';
+          + '\n\n'
 
-        break;
+        break
       case NOTION_BLOCK_TYPES.divider:
-        result += '---\n\n';
+        result += '---\n\n'
 
-        break;
+        break
       default:
-        throw new Error(`Unknown block type: ${block.type}`);
+        throw new Error(`Unknown block type: ${block.type}`)
     }
 
   }
 
-  return _.trim(result);
+  return _.trim(result)
 }
 
 /**
