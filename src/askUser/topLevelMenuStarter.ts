3a347a5da14797de54aebc31b51db0408bcf04af
Ivan <ipkozyrin@gmail.com>
2023-02-11 14:00:39 +0300

    change

--- src/askUser/topLevelMenuStarter.ts
+++ src/askUser/topLevelMenuStarter.ts
@@ -8,16 +8,6 @@ import {startTelegraphMenu} from './telegraph/startTelegraphMenu.js';
 export async function topLevelMenuStarter(tgChat: TgChat) {
   return askMainMenu(tgChat, tgChat.asyncCb(async (blogNameOrAction: string) => {
 
-    // tgChat.app.tg.bot.telegram.sendMessage(
-    //   tgChat.botChatId,
-    //   convertCommonMdToTgHtml(
-    //   '\n\nnorm *bold _italic2_*\n _italic_ __underiline__ ~strikethrough~ `monospace`  [https://google.com](https://google.com) [url](https://google.com/) norm'
-    //   ) || '',
-    //   {
-    //     parse_mode: 'HTML'
-    //   }
-    // )
-
     switch (blogNameOrAction) {
       case MAIN_MENU_ACTION.TASKS:
         return startTaskMenu(tgChat);
