415cd47ff13259f862f2a464df08d0e52ec3d0dd
Ivan <ipkozyrin@gmail.com>
2023-02-16 17:13:19 +0300

    change

--- src/askUser/topLevelMenuStarter.ts
+++ src/askUser/topLevelMenuStarter.ts
@@ -1,7 +1,7 @@
 import TgChat from '../apiTg/TgChat.js';
 import {askMainMenu, MAIN_MENU_ACTION} from './askMainMenu.js';
 import {startBlogMenu} from './startBlogMenu.js';
-import {startTaskMenu} from './task/startTaskMenu.js';
+import {startTaskListMenu} from './task/startTaskListMenu.js';
 import {startTelegraphMenu} from './telegraph/startTelegraphMenu.js';
 
 
@@ -10,7 +10,7 @@ export async function topLevelMenuStarter(tgChat: TgChat) {
 
     switch (blogNameOrAction) {
       case MAIN_MENU_ACTION.TASKS:
-        return startTaskMenu(tgChat);
+        return startTaskListMenu(tgChat);
       case MAIN_MENU_ACTION.TELEGRAPH:
         return startTelegraphMenu(tgChat);
       default:
