c3e4af9abcbfeea456a08c30e06d1f914983a428
Ivan <ipkozyrin@gmail.com>
2023-02-22 22:37:03 +0300

    change

--- src/notionRequests/createBuyAdItem.ts
+++ src/notionRequests/createBuyAdItem.ts
@@ -3,31 +3,6 @@ import TgChat from '../apiTg/TgChat.js';
 import {AD_BUY_TYPES, AD_FORMATS} from '../types/types.js';
 
 
-// TODO: это нафига???
-const BUY_AD_TYPE_IDS: Record<keyof typeof AD_BUY_TYPES, string> = {
-  bestArticles: 'ZpdT',
-  questionSolve: 'Th{s',
-  advert: '?QvM',
-  recommend: 'h{Sb',
-};
-
-// TODO: это нафига???
-const BUY_AD_FORMAT_IDS: Record<keyof typeof AD_FORMATS, string> = {
-  '1/24': 'CYmi',
-  '1/48': 'oR::',
-  '2/24': 'Ngws',
-  '2/48': 'N[vW',
-  '2/72': 'd?iO',
-  '3/24': 'I}Um',
-  '3/48': 'ZPsl',
-  '3/72': 'gSsX',
-  'full/24': ']?}Y',
-  'full/48': 'v?mt',
-  'full/72': 'Iov}',
-  forever: 'CX}m',
-};
-
-
 export async function createBuyAdItem(
   blogName: string,
   tgChat: TgChat,
@@ -35,21 +10,13 @@ export async function createBuyAdItem(
   time: string,
   adType: keyof typeof AD_BUY_TYPES,
   format: keyof typeof AD_FORMATS,
-  channelName: string,
+  channelName?: string,
   channelUrl?: string,
   cost?: number,
   note?: string,
 ) {
+
   // TODO: note - преобразовать html в notion
-
-  // TODO: вместо этого выгрузить данные из notion
-
-  const adTypeId: string = BUY_AD_TYPE_IDS[adType];
-
-  // TODO: вместо этого выгрузить данные из notion
-
-  const formatId: string = BUY_AD_FORMAT_IDS[format]
-
 
   const request: CreatePageParameters = {
     parent: { database_id: tgChat.app.blogs[blogName].notion.buyTgDbId },
@@ -72,16 +39,16 @@ export async function createBuyAdItem(
       ad_type: {
         type: 'select',
         select: {
-          id: adTypeId,
+          name: adType,
         }
       },
       format: {
         type: 'select',
         select: {
-          id: formatId,
+          name: format,
         }
       },
-      channel: {
+      channel: (channelName) ? {
         type: 'rich_text',
         rich_text: [{
           type: 'text',
@@ -90,7 +57,7 @@ export async function createBuyAdItem(
             link: (channelUrl) ? { url: channelUrl } : undefined,
           },
         }],
-      },
+      } : (undefined as any),
     },
   };
 
@@ -116,3 +83,47 @@ export async function createBuyAdItem(
 
   if (!result.id) throw new Error(`No result id`)
 }
+
+
+
+// const dbResponse = await tgChat.app.notion.api.databases.retrieve({
+//   database_id: tgChat.app.blogs[blogName].notion.buyTgDbId
+// })
+//
+// const foundFormat = (dbResponse.properties['format'] as any).select.options
+//   .find((item: {id: string, name: string}) => {
+//
+//   })
+//
+// if (!foundFormat) {
+//   throw new Error(`Can't find specified format id`)
+// }
+//
+// const formatId: string = BUY_AD_FORMAT_IDS[format]
+//
+// console.log(111, ()
+//
+//
+// const adTypeId: string = BUY_AD_TYPE_IDS[adType];
+
+// const BUY_AD_TYPE_IDS: Record<keyof typeof AD_BUY_TYPES, string> = {
+//   bestArticles: 'ZpdT',
+//   questionSolve: 'Th{s',
+//   advert: '?QvM',
+//   recommend: 'h{Sb',
+// };
+//
+// const BUY_AD_FORMAT_IDS: Record<keyof typeof AD_FORMATS, string> = {
+//   '1/24': 'CYmi',
+//   '1/48': 'oR::',
+//   '2/24': 'Ngws',
+//   '2/48': 'N[vW',
+//   '2/72': 'd?iO',
+//   '3/24': 'I}Um',
+//   '3/48': 'ZPsl',
+//   '3/72': 'gSsX',
+//   'full/24': ']?}Y',
+//   'full/48': 'v?mt',
+//   'full/72': 'Iov}',
+//   forever: 'CX}m',
+// };
