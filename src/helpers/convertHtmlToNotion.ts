f2d3218e80ceb6051f6281f4bfa16c9777beee40
Ivan <ipkozyrin@gmail.com>
2023-02-23 20:42:18 +0300

    change

--- src/helpers/convertHtmlToNotion.ts
+++ src/helpers/convertHtmlToNotion.ts
@@ -1,7 +1,25 @@
 import {RichTextItemResponse} from '@notionhq/client/build/src/api-endpoints.js';
 
+type RichTextItemRequest = {
+  text: {
+    content: string;
+    link?: {
+      url: string;
+    } | null;
+  };
+  type?: "text";
+  annotations?: {
+    bold?: boolean;
+    italic?: boolean;
+    strikethrough?: boolean;
+    underline?: boolean;
+    code?: boolean;
+    color?: "default" | "gray" | "brown" | "orange" | "yellow" | "green" | "blue" | "purple" | "pink" | "red" | "gray_background" | "brown_background" | "orange_background" | "yellow_background" | "green_background" | "blue_background" | "purple_background" | "pink_background" | "red_background";
+  };
+}
 
-export function convertHtmlToNotion(htmlText: string): RichTextItemResponse[] {
+
+export function convertHtmlToNotion(htmlText: string): RichTextItemRequest[] {
   return [
     {
       type: 'text',
@@ -15,10 +33,7 @@ export function convertHtmlToNotion(htmlText: string): RichTextItemResponse[] {
         strikethrough: false,
         underline: false,
         code: false,
-        color: 'default',
       },
-      plain_text: htmlText,
-      href: null
     }
   ]
 }
