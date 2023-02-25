b6a545a0e0ae2a28a5e0d983587c7432438283b7
Ivan <ipkozyrin@gmail.com>
2023-02-25 12:01:18 +0300

    change

---
+++ _useless/BloggerComMain--old.ts
@@ -0,0 +1,84 @@
+//import {blogger_v3, google} from 'googleapis'
+
+
+interface AccessJson {
+  clientId: string,
+  clientSecret: string,
+  access_token: string,
+  refresh_token: string,
+}
+
+
+export default class BloggerComMain {
+  private readonly accessObj: AccessJson
+  //private blogger!: blogger_v3.Blogger
+
+
+  constructor(accessObjStr: string) {
+    this.accessObj = JSON.parse(accessObjStr)
+  }
+
+  async init() {
+    // const oauth2Client = new google.auth.OAuth2(
+    //   this.accessObj.clientId,
+    //   this.accessObj.clientSecret,
+    //   'https://oauth2.googleapis.com/token'
+    // )
+    //
+    // oauth2Client.setCredentials({
+    //   access_token: this.accessObj.access_token,
+    //   refresh_token: this.accessObj.refresh_token,
+    // })
+    //
+    // this.blogger = new blogger_v3.Blogger({ auth: oauth2Client })
+  }
+
+  async createPost(
+    blogId: string,
+    title: string,
+    content: string,
+    // like '2023-02-24T13:05:00+03:00'
+    publishIsoDateTime: string,
+    labels?: string[],
+    isDraft = false
+  ): Promise<any> {
+  //): Promise<blogger_v3.Schema$Post> {
+    return {} as any
+
+    // const res = await this.blogger.posts.insert({
+    //   blogId,
+    //   isDraft,
+    //   requestBody: {
+    //     title,
+    //     content,
+    //     labels,
+    //     published: publishIsoDateTime,
+    //     // "images": [
+    //     //   {
+    //     //     "url": string
+    //     //
+    //     //
+    //     //   }
+    //     // ],
+    //   }
+    // })
+    //
+    // if (res.status !== 200) {
+    //   throw new Error(`Can't create a new post on blogger.com: ${res.status} ${res.statusText}`)
+    // }
+    //
+    // return res.data
+  }
+}
+
+
+
+
+// (async () => {
+//   const bloggerCom = new BloggerComMain(
+//     process.env['GOOGLE_API_TOKENS'] || '{}',
+//   )
+//
+//   await bloggerCom.init()
+//   await bloggerCom.createPost('', '', '')
+// })()
--- src/apiBloggerCom/BloggerComMain.ts
+++ src/apiBloggerCom/BloggerComMain.ts
@@ -1,17 +1,43 @@
-//import {blogger_v3, google} from 'googleapis'
+import {OAuth2Client} from 'google-auth-library'
+import {GaxiosError} from 'gaxios'
 
+/**
+ * See
+ * * https://www.npmjs.com/package/google-auth-library
+ * * https://developers.google.com/blogger/docs/3.0/using?hl=ru#AddingAPost
+ * * https://developers.google.com/blogger/docs/3.0/reference/posts/insert?hl=ru
+ * * https://console.cloud.google.com/apis/credentials
+ */
+
+
+interface BloggerResponseData {
+  kind: 'blogger#post'
+  id: string
+  // 'DRAFT'
+  status: string,
+  blog: { id: string },
+  // like 2023-02-25T11:44:00+03:00
+  published: string
+  // like 2023-02-25T11:44:36+03:00
+  updated: string
+  // is http://blog.slsfreedom.org/
+  url: string
+  // like https://www.googleapis.com/blogger/v3/blogs/4309559355980291324/posts/8324745815131022600
+  selfLink: string
+  title: string
+  content: string
+}
 
 interface AccessJson {
   clientId: string,
   clientSecret: string,
-  access_token: string,
-  refresh_token: string,
+  refreshToken: string,
 }
 
 
 export default class BloggerComMain {
   private readonly accessObj: AccessJson
-  //private blogger!: blogger_v3.Blogger
+  private client!: OAuth2Client
 
 
   constructor(accessObjStr: string) {
@@ -19,18 +45,7 @@ export default class BloggerComMain {
   }
 
   async init() {
-    // const oauth2Client = new google.auth.OAuth2(
-    //   this.accessObj.clientId,
-    //   this.accessObj.clientSecret,
-    //   'https://oauth2.googleapis.com/token'
-    // )
-    //
-    // oauth2Client.setCredentials({
-    //   access_token: this.accessObj.access_token,
-    //   refresh_token: this.accessObj.refresh_token,
-    // })
-    //
-    // this.blogger = new blogger_v3.Blogger({ auth: oauth2Client })
+    this.client = await this.doAuth()
   }
 
   async createPost(
@@ -38,37 +53,76 @@ export default class BloggerComMain {
     title: string,
     content: string,
     // like '2023-02-24T13:05:00+03:00'
-    publishIsoDateTime: string,
+    publishIsoDateTime?: string,
     labels?: string[],
     isDraft = false
-  ): Promise<any> {
-  //): Promise<blogger_v3.Schema$Post> {
-    return {} as any
+  ): Promise<BloggerResponseData> {
+    const url = `https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts/`
+    const data = {
+      title,
+      content,
+      labels,
+      published: publishIsoDateTime,
+      // "images": [
+      //   {
+      //     "url": string
+      //
+      //
+      //   }
+      // ],
+    }
 
-    // const res = await this.blogger.posts.insert({
-    //   blogId,
-    //   isDraft,
-    //   requestBody: {
-    //     title,
-    //     content,
-    //     labels,
-    //     published: publishIsoDateTime,
-    //     // "images": [
-    //     //   {
-    //     //     "url": string
-    //     //
-    //     //
-    //     //   }
-    //     // ],
-    //   }
-    // })
-    //
-    // if (res.status !== 200) {
-    //   throw new Error(`Can't create a new post on blogger.com: ${res.status} ${res.statusText}`)
-    // }
-    //
-    // return res.data
+    return this.doPostRequest(url, data, isDraft)
   }
+
+  private async doPostRequest(
+    url: string,
+    data: Record<string, any>,
+    isDraft?: boolean
+  ): Promise<BloggerResponseData> {
+    try {
+      const res = await this.client.request({
+        url,
+        method: 'POST',
+        responseType: 'json',
+        params: { isDraft },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data,
+      })
+
+      return res.data as BloggerResponseData
+    }
+    catch (e) {
+      const gaxiosErr = e as GaxiosError
+
+      throw new Error(`Status: ${gaxiosErr.response?.status}, ${gaxiosErr.response?.statusText}`)
+    }
+
+  }
+
+  private async doAuth(): Promise<OAuth2Client> {
+    const oAuth2Client = new OAuth2Client(
+      this.accessObj.clientId,
+      this.accessObj.clientSecret,
+      'https://oauth2.googleapis.com/token'
+    )
+
+    oAuth2Client.setCredentials({
+      refresh_token: this.accessObj.refreshToken
+    })
+
+    // // Generate the url that will be used for the consent dialog.
+    // const authorizeUrl = oAuth2Client.generateAuthUrl({
+    //   access_type: 'offline',
+    //   scope: 'https://www.googleapis.com/auth/userinfo.profile',
+    // });
+    // console.log(authorizeUrl)
+
+    return oAuth2Client
+  }
+
 }
 
 
@@ -80,5 +134,12 @@ export default class BloggerComMain {
 //   )
 //
 //   await bloggerCom.init()
-//   await bloggerCom.createPost('', '', '')
+//   await bloggerCom.createPost(
+//     '4309559355980291324',
+//     'some title',
+//     '<p>some content</p>',
+//     undefined,
+//     undefined,
+//     true
+//   )
 // })()
