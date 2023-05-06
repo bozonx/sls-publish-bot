a71af565abd4b9bcfbed397736bba053f7eec59d
Ivan <ipkozyrin@gmail.com>
2023-05-06 18:48:13 -0300

    add

--- src/DynamicMenu/DynamicMenuFactory.ts
+++ src/DynamicMenu/DynamicMenuFactory.ts
@@ -1,9 +1,30 @@
 import {DynamicMenuInstance} from './DynamicMenuInstance.js';
 
 
+export type MenuChangeHandler = (menuDefinition: MenuDefinition) =>
+  (undefined | MenuItem[][] | Promise<MenuItem[][]>)
+
+
+
 export class DynamicMenuFactory {
   private instances: DynamicMenuInstance[] = []
+  private registeredHandlers: MenuChangeHandler[] = []
 
+
+  constructor() {
+
+  }
+
+  async destroy() {
+    this.registeredHandlers = []
+
+    // TODO: destroy all the instances
+  }
+
+
+  getInstance(instanceId: string): DynamicMenuInstance | undefined {
+    return this.instances[instanceId]
+  }
 
   makeInstance<InstanceContext = Record<any, any>>(instanceContext: InstanceContext): DynamicMenuInstance {
     const instanceId = String(this.instances.length)
@@ -12,6 +33,16 @@ export class DynamicMenuFactory {
     this.instances.push(newInstance)
 
     return newInstance
+  }
+
+  registerMenuHandler(handler: MenuChangeHandler): string {
+    this.registeredHandlers.push(handler)
+
+    return String(this.registeredHandlers.length - 1)
+  }
+
+  removeHandler(handlerIndex: string) {
+    delete this.registeredHandlers[handlerIndex]
   }
 
 
