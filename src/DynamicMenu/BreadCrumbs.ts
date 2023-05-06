4c133bc96a7cec73be67aeb1dd03e5023e79a014
Ivan <ipkozyrin@gmail.com>
2023-05-06 19:41:16 -0300

    add

--- src/DynamicMenu/BreadCrumbs.ts
+++ src/DynamicMenu/BreadCrumbs.ts
@@ -19,7 +19,7 @@ export interface BreadCrumbsStep {
 export default class BreadCrumbs {
   pathChangeEvent: IndexedEvents<() => void> = new IndexedEvents()
   private currentStepId: string = '0'
-  private readonly steps: BreadCrumbsStep[] = []
+  private steps: BreadCrumbsStep[] = []
 
 
   constructor() {
@@ -31,6 +31,23 @@ export default class BreadCrumbs {
   }
 
 
+  getCurrentPath(): string {
+    return this.getPathOfStepId(this.currentStepId)
+  }
+
+  getPathOfStepId(stepId: string): string {
+    const names: string[] = this.steps.map((el) => el.name)
+    // TODO: проверить
+    const sliced: string[] = names.splice(0, Number(this.currentStepId) + 1)
+
+    return sliced.join(BREADCRUMBS_DELIMITER)
+  }
+
+  /**
+   * Add step to current path
+   * @param name
+   * @param initialState
+   */
   addStep(name: string, initialState: Record<any, any> = {}): string {
     const stepId = String(this.steps.length)
 
@@ -46,16 +63,25 @@ export default class BreadCrumbs {
     return this.currentStepId
   }
 
-  getCurrentPath(): string {
-    return this.getPathOfStepId(this.currentStepId)
-  }
+  toPath(newPath: string): string {
+    this.steps = []
 
-  getPathOfStepId(stepId: string): string {
-    const names: string[] = this.steps.map((el) => el.name)
-    // TODO: проверить
-    const sliced: string[] = names.splice(0, Number(this.currentStepId) + 1)
+    const pathNames: string[] = newPath.split(BREADCRUMBS_DELIMITER)
 
-    return sliced.join(BREADCRUMBS_DELIMITER)
+    // TODO: что будет в случае рута???
+
+    for (const name of pathNames) {
+      this.steps.push({
+        name,
+        state: {},
+      })
+    }
+
+    this.currentStepId = String(pathNames.length - 1)
+
+    this.pathChangeEvent.emit()
+
+    return this.currentStepId
   }
 
   // /**
