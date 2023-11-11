4a39a2eeb9c6fa055abb9f34bacc757b8c575bf9
Ivan <ipkozyrin@gmail.com>
2023-11-11 13:49:10 -0300

    WIP

--- src/lib/squidletAppApi.ts
+++ src/lib/squidletAppApi.ts
@@ -54,36 +54,11 @@ let squidletUi: SquidletAppApiConnection | undefined
 export const squidletAppApi = {
   async loadAllBlogs(): Promise<ListResponse<BlogMeta>> {
     const result = []
-
     const {data: blogsArr} = await squidletUi.app.ctx.home.readDir(PUBLISHER_ROOT_DIR)
-
-    console.log(33333, blogsArr)
-
-    // const resp = await squidletUi?.send({
-    //   data: {
-    //     method: 'ctx.home.readDir',
-    //     arguments: [PUBLISHER_ROOT_DIR],
-    //   },
-    // })
-    //
-    // if (resp.errorStatus) {
-    //   throw error(resp.errorStatus, resp.errorMessage)
-    // }
-
 
     for (const dirName of blogsArr) {
       const blogYamlPath = pathJoin(PUBLISHER_ROOT_DIR, dirName, BLOG_YAML)
       const {data: yamlStr} = await squidletUi.app.ctx.home.readTextFile(blogYamlPath)
-      // const blogResp = await squidletUi?.send({
-      //   data: {
-      //     method: 'ctx.home.readTextFile',
-      //     arguments: [blogYamlPath],
-      //   }
-      // })
-      //
-      // if (blogResp.errorStatus) {
-      //   throw error(blogResp.errorStatus, blogResp.errorMessage)
-      // }
 
       let yamlData: any
 
@@ -111,21 +86,12 @@ export const squidletAppApi = {
 
   async loadBlogData(blogName: string): Promise<ItemResponse<BlogMeta>> {
     const blogYamlPath = pathJoin(PUBLISHER_ROOT_DIR, blogName, BLOG_YAML)
-    const blogResp = await squidletUi?.send({
-      data: {
-        method: 'ctx.home.readTextFile',
-        arguments: [blogYamlPath],
-      }
-    })
-
-    if (blogResp.errorStatus) {
-      throw error(blogResp.errorStatus, blogResp.errorMessage)
-    }
+    const {data: yamlStr} = await squidletUi.app.ctx.home.readTextFile(blogYamlPath)
 
     let yamlData: any
 
     try {
-      yamlData = yaml.parse(blogResp.data)
+      yamlData = yaml.parse(yamlStr)
     }
     catch (e) {
       throw error(500, `Error parsing yaml of "${blogYamlPath}": ${e}`)
@@ -139,48 +105,20 @@ export const squidletAppApi = {
     }
   },
 
-  // async loadBlogConfig(blogName: string): Promise<ItemResponse<BlogConfig>> {
-  //   const blogConfigPath = pathJoin(PUBLISHER_ROOT_DIR, blogName, TO_PUBLISH_DIR)
-  //   const resp = await squidletUi?.send({
-  //     method: 'ctx.home.readDir',
-  //     arguments: [toPublishDirPath],
-  //   })
-  // },
-
   async loadBlogPosts(blogName: string): Promise<ListResponse<PostResult>> {
     const result = []
     const toPublishDirPath = pathJoin(PUBLISHER_ROOT_DIR, blogName, TO_PUBLISH_DIR)
-    const resp = await squidletUi?.send({
-      data: {
-        method: 'ctx.home.readDir',
-        arguments: [toPublishDirPath],
-      },
-    })
-
-    if (resp.errorStatus) {
-      throw error(resp.errorStatus, resp.errorMessage)
-    }
-    
-    const filtered = resp.data.filter((item: string) => item.match(/\.md$/))
+    const {data: blogsArr} = await squidletUi.app.ctx.home.readDir(toPublishDirPath)
+    const filtered = blogsArr.filter((item: string) => item.match(/\.md$/))
     
     for (const mdFileName of filtered) {
       const mdFilePath = pathJoin(toPublishDirPath, mdFileName)
-      const mdFileResp = await squidletUi?.send({
-        data: {
-          method: 'ctx.home.readTextFile',
-          arguments: [mdFilePath],
-        }
-      })
-
-      if (mdFileResp.errorStatus) {
-        throw error(mdFileResp.errorStatus, mdFileResp.errorMessage)
-      }
-
+      const {data: yamlStr} = await squidletUi.app.ctx.home.readTextFile(mdFilePath)
       let meta: any
       let md: string
 
       try {
-        [meta, md] = await splitMetaMd(mdFileResp.data)
+        [meta, md] = await splitMetaMd(yamlStr)
       }
       catch (e) {
         throw error(500, `Error parsing yaml of "${mdFilePath}": ${e}`)
@@ -213,22 +151,13 @@ export const squidletAppApi = {
       TO_PUBLISH_DIR,
       postFileName
     )
-    const mdFileResp = await squidletUi?.send({
-      data: {
-        method: 'ctx.home.readTextFile',
-        arguments: [mdFilePath],
-      },
-    })
-
-    if (mdFileResp.errorStatus) {
-      throw error(mdFileResp.errorStatus, mdFileResp.errorMessage)
-    }
+    const {data: mdStr} = await squidletUi.app.ctx.home.readTextFile(mdFilePath)
 
     let meta: any
     let md: string
 
     try {
-      [meta, md] = await splitMetaMd(mdFileResp.data)
+      [meta, md] = await splitMetaMd(mdStr)
     }
     catch (e) {
       throw error(500, `Error parsing yaml of "${mdFilePath}": ${e}`)
@@ -254,72 +183,36 @@ export const squidletAppApi = {
       BLOG_YAML,
     )
     const yamlStr = yaml.stringify(config)
-    const mdFileResp = await squidletUi?.send({
-      data: {
-        method: 'ctx.home.writeFile',
-        arguments: [blogYamlPath, yamlStr],
-      },
-    })
 
-    if (mdFileResp.errorStatus) {
-      throw error(mdFileResp.errorStatus, mdFileResp.errorMessage)
-    }
+    await squidletUi.app.ctx.home.writeFile(blogYamlPath, yamlStr)
   },
 
   /**
    * Load app config. It creates it if it not exists
    */
   async loadAppConfig(): Promise<ItemResponse<AppConfig>> {
-    const existsResp = await squidletUi?.send({
-      data: {
-        method: 'ctx.cfgSynced.isExists',
-        arguments: [APP_CONFIG_YAML],
-      }
-    })
+    const {data: isExists} = await squidletUi.app.ctx.cfgSynced.isExists(APP_CONFIG_YAML)
 
-    if (existsResp.errorStatus) throw error(existsResp.errorStatus, existsResp.errorMessage)
-
-    if (existsResp.data === false) {
-
+    if (isExists === false) {
       // TODO: может это в squidlet делать ???
-
       // make config for Publisher app
-      const mkdirpResp = await squidletUi?.send({
-        data: {
-          method: 'ctx.cfgSynced.mkDirP',
-          arguments: ['/'],
-        }
-      })
-
-      if (mkdirpResp.errorStatus) throw error(mkdirpResp.errorStatus, mkdirpResp.errorMessage)
-
-      const createResp = await squidletUi?.send({
-        data: {
-          method: 'ctx.cfgSynced.writeFile',
-          arguments: [APP_CONFIG_YAML, yaml.stringify(APP_CONFIG_DEFAULTS)],
-        }
-      })
-
-      if (createResp.errorStatus) throw error(createResp.errorStatus, createResp.errorMessage)
+      await squidletUi.app.ctx.cfgSynced.mkDirP('/')
+      await squidletUi.app.ctx.cfgSynced.writeFile(
+        APP_CONFIG_YAML,
+        yaml.stringify(APP_CONFIG_DEFAULTS)
+      )
 
       return {
         result: APP_CONFIG_DEFAULTS,
       }
     }
 
-    const loadResp = await squidletUi?.send({
-      data: {
-        method: 'ctx.cfgSynced.readTextFile',
-        arguments: [APP_CONFIG_YAML],
-      }
-    })
-
-    if (loadResp.errorStatus) throw error(loadResp.errorStatus, loadResp.errorMessage)
+    const {data: yamlStr} = await squidletUi.app.ctx.cfgSynced.readTextFile(APP_CONFIG_YAML)
 
     let yamlData: any
 
     try {
-      yamlData = yaml.parse(loadResp.data)
+      yamlData = yaml.parse(yamlStr)
     }
     catch (e) {
       throw error(500, `Error parsing yaml of "${APP_CONFIG_YAML}": ${e}`)
@@ -332,14 +225,8 @@ export const squidletAppApi = {
 
   async saveAppConfig(config: AppConfig): Promise<void> {
     const yamlStr = yaml.stringify(config)
-    const writeResp = await squidletUi?.send({
-      data: {
-        method: 'ctx.cfgSynced.writeFile',
-        arguments: [APP_CONFIG_YAML, yamlStr],
-      }
-    })
 
-    if (writeResp.errorStatus) throw error(writeResp.errorStatus, writeResp.errorMessage)
+    await squidletUi.app.ctx.cfgSynced.writeFile(APP_CONFIG_YAML, yamlStr)
   }
 
 }
