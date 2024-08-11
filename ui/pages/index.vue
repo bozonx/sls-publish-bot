9b1befdfe6c7e82ec568d1ae5ea2d19cd54c1f0d
Ivan <ipkozyrin@gmail.com>
2024-08-10 22:40:56 -0300

    WIP

---
+++ ui/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>index</div>
+  <ul>
+    <li>
+      <NuxtLink to="/inbox">Manage inbox</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/publish-article">Publish article</NuxtLink>
+    </li>
+  </ul>
+  <ListItem />
+</template>
