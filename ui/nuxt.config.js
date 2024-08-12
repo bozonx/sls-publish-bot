71e63b73d0fff6f8638fc160c474ddbb1f75ffa9
Ivan <ipkozyrin@gmail.com>
2024-08-12 10:20:29 -0300

    WIP

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -9,6 +9,7 @@ import { definePreset } from "@primevue/themes";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: "./src",
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   $production: {},
