0245a93f8aef95775f26f61c8970627ebb399a42
Ivan <ipkozyrin@gmail.com>
2024-08-24 22:03:50 -0300

    WIP

--- worker/src/apiIndex.js
+++ worker/src/apiIndex.js
@@ -1,5 +1,7 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { sessionMiddleware, CookieStore } from 'hono-sessions';
+import { CloudflareD1Store } from 'hono-sessions/cloudflare-d1-store';
 import apiTgBot from './apiTgBot.js';
 import apiUser from './apiUser.js';
 import apiWorkspace from './apiWorkspace.js';
@@ -7,6 +9,24 @@ import apiBlog from './apiBlog.js';
 import apiInbox from './apiInbox.js';
 
 const app = new Hono().basePath('/api');
+let store;
+
+app.use('*', (c, next) => {
+	if (!store) store = new CloudflareD1Store();
+
+	store.db = c.env.DB;
+
+	return sessionMiddleware({
+		store,
+		encryptionKey: 'Cw7j^#Yj7%tVocy2Pp7GCw7j^#Yj7%tVocy2Pp7G',
+		expireAfterSeconds: 900, // Expire session after 15 minutes of inactivity
+		cookieOptions: {
+			sameSite: 'Lax', // Recommended for basic CSRF protection in modern browsers
+			path: '/', // Required for this library to work properly
+			httpOnly: true, // Recommended to avoid XSS attacks
+		},
+	})(c, next);
+});
 
 app.use(
 	'*',
